97200469d60f467174dc1903c8a3c5c9acee683f
kristindyulgeryan <157119345+kristindyulgeryan@users.noreply.github.com>
2025-01-07 13:29:18 +0200

    Update app.js

--- Fun Facts-Resources/app.js
+++ Fun Facts-Resources/app.js
@@ -1,15 +1,3 @@
-// import { logout } from "./data/user.js";
-// import { page } from "./lib.js";
-// import { updateNav } from "./util.js";
-// import { createView } from "./views/createView.js";
-// import { dashboardView } from "./views/dashboardView.js";
-// import { detailsView } from "./views/detailsView.js";
-// import { editView } from "./views/editView.js";
-// import { homeView } from "./views/homeView.js";
-// import { loginView } from "./views/loginView.js";
-// import { registerView } from "./views/registerView.js";
-// import { searchView } from "./views/searchView.js";
-
 import { logout } from "./data/user.js";
 import { page } from "./lib.js";
 import { updateNav } from "./util.js";
@@ -21,9 +9,7 @@ import { homeView } from "./views/homeView.js";
 import { loginView } from "./views/loginView.js";
 import { registerView } from "./views/registerView.js";
 
-// updateNav();
 updateNav();
-// page("/", homeView);
 page("/", homeView);
 page("/login", loginView);
 page("/register", registerView);
